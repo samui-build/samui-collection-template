4b89c3349b05bd4eac54a7ea876efe684f0fc742
Bram Borggreve <beeman@beeman.nl>
2025-01-29 21:32:15 +0000

    chore: initial commit

---
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -0,0 +1,17 @@
+import { LucideMoon, LucideSun } from 'lucide-react';
+import { ActionIcon, ActionIconProps, useMantineColorScheme } from '@mantine/core';
+
+export function ColorSchemeToggle(props: ActionIconProps) {
+  const { setColorScheme, colorScheme } = useMantineColorScheme();
+  const isDark = colorScheme === 'dark';
+
+  return (
+    <ActionIcon
+      onClick={() => setColorScheme(isDark ? 'light' : 'dark')}
+      variant="light"
+      {...props}
+    >
+      {isDark ? <LucideSun size={20} /> : <LucideMoon size={20} />}
+    </ActionIcon>
+  );
+}
